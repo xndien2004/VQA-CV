a7717fdfd3ad33f3afbe4a6049834565b81009ef
fitiuh <xuandienk4@gmail.com>
2026-02-16 05:22:59 +0000

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -26,20 +26,20 @@ python3 -m VQA-CV.train \
     --max_dev_samples -1 \
     2>&1 | tee train0.6B.log
 
-# python3 -m VQA-CV.train \
-# 	--llm_name Qwen/Qwen3-0.6B \
-#     --image_encoder_name google/siglip2-so400m-patch16-naflex \
-#     --vision_projector_type mlp_prefix \
-#     --train_path /home/fit02/dien_workspace/vqa/dataset/vitextvqa/ViTextVQA_train.json \
-#     --dev_path /home/fit02/dien_workspace/vqa/dataset/vitextvqa/ViTextVQA_dev.json \
-#     --image_root /home/fit02/dien_workspace/vqa/dataset/vitextvqa/images/st_images \
-#     --caption_path /home/fit02/dien_workspace/vqa/dataset/vitextvqa/vitextvqa_captions.json \
-#     --epochs 50 \
-#     --batch_size 4 \
-#     --lr 2e-5 \
-#     --patience 3 \
-#     --num_workers 6 \
-#     --checkpoint_dir outputs_vitext_noOCR/ \
-#     --max_train_samples -1 \
-#     --max_dev_samples -1 \
-#     2>&1 | tee train0.6BnoOCR.log
+python3 -m VQA-CV.train \
+	--llm_name Qwen/Qwen3-0.6B \
+    --image_encoder_name google/siglip2-so400m-patch16-naflex \
+    --train_path /home/fit02/dien_workspace/vqa/dataset/viocrvqa/train.json \
+    --dev_path /home/fit02/dien_workspace/vqa/dataset/viocrvqa/dev.json \
+    --image_root /home/fit02/dien_workspace/vqa/dataset/viocrvqa/images \
+    --caption_path /home/fit02/dien_workspace/vqa/dataset/viocrvqa/viocrvqa_captions.json \
+    --ocr_path /home/fit02/dien_workspace/vqa/dataset/viocrvqa/docr_features_of_viocr.npy \
+    --epochs 50 \
+    --batch_size 2 \
+    --lr 2e-5 \
+    --patience 3 \
+    --num_workers 6 \
+    --checkpoint_dir outputs_viocr/ \
+    --max_train_samples -1 \
+    --max_dev_samples -1 \
+    2>&1 | tee train0.6B_viocr.log
