f3fa1be43302a40494d61b5274a3d2abac63760e
xndien2004 <xuandienk4@gmail.com>
2026-02-19 19:12:13 +0700

    update

--- models/language_model/qwen.py
+++ models/language_model/qwen.py
@@ -1,25 +1,24 @@
 from typing import List, Optional, Tuple
 
 import torch
-from transformers.models.qwen3.modeling_qwen3 import Qwen3Model, Qwen3Config, Qwen3ForCausalLM
+from transformers.models.qwen2.modeling_qwen2 import Qwen2Model, Qwen2Config, Qwen2ForCausalLM
 
 from ..vivqa_arch import ViVQAMetaForCausalLM, ViVQAMetaModel
 
 
-class ViVQAConfig(Qwen3Config):
+class ViVQAConfig(Qwen2Config):
     model_type = "vivqa"
 
 
-class ViVQAModel(Qwen3Model, ViVQAMetaModel):
-    """Backbone Qwen3 + module vision (SigLIP + projector)."""
+class ViVQAModel(Qwen2Model, ViVQAMetaModel):
     config_class = ViVQAConfig
 
-    def __init__(self, config: Qwen3Config):
-        Qwen3Model.__init__(self, config)
+    def __init__(self, config: Qwen2Config):
+        Qwen2Model.__init__(self, config)
         ViVQAMetaModel.__init__(self, config)
 
 
-class ViVQAForCausalLM(Qwen3ForCausalLM, ViVQAMetaForCausalLM):
+class ViVQAForCausalLM(Qwen2ForCausalLM, ViVQAMetaForCausalLM):
 
     config_class = ViVQAConfig
 
